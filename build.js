6e096b65fd49b74fffc508cced4e5281becd4c9d
H.Alper Tuna <halpertuna@gmail.com>
2016-06-22 20:43:33 +0300

    Added build tools.

---
+++ build.js
@@ -0,0 +1,35 @@
+({
+    paths: {
+        'burner': 'js'
+    },
+    include:[
+        'burner/third/date.format',
+        'burner/core/Utils',
+        'burner/core/EventHandler',
+        'burner/core/createClass',
+        'burner/core/AjaxGroup',
+        'burner/core/Ajax',
+        'burner/ui/Element',
+        'burner/ui/Switch',
+        'burner/ui/TextElement',
+        'burner/ui/iInput',
+        'burner/ui/Group',
+        'burner/ui/Icon',
+        'burner/ui/Popup',
+        'burner/ui/CheckGroup',
+        'burner/ui/Input',
+        'burner/ui/Button',
+        'burner/ui/Label',
+        'burner/ui/Notifier',
+        'burner/ui/Document',
+        'burner/ui/Spinner',
+        'burner/ui/DatePicker',
+        'burner/ui/Check',
+        'burner/ui/Tip',
+        'burner/ui/TimePicker',
+        'burner/ui/RadioGroup',
+        'burner/ui/DropdownPopup',
+        'burner/ui/Dropdown',
+        'burner/ui/DatePickerPopup'
+    ]
+})
